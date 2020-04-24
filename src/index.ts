672fdce6cd57cae223687856b9860ea10008f89f
Hxiaowei <861592174@qq.com>
2020-04-24 16:52:16 +0800

    get params url

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import { AxiosRequestConfig } from './types/index'
 import xhr from './xhr'
+// import {} from '../'
 
-function axios(config: AxiosRequestConfig) {
+function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
+}
+function processConfig(config: AxiosRequestConfig) {
+  config.url = transformUrl(config)
+}
+
+function transformUrl(config: AxiosRequestConfig) {
+  const { url, params } = config
+  return buildUrl(url, params)
 }
 
 export default axios
